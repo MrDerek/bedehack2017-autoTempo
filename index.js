dc11b6c571d7cc52b9743f16cd57364053b584ab
venkatadegala <venkata.degala@bedegaming.com>
2017-12-07 10:31:12 +0000

    Add Basic code

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch();
+async function run() {
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+  
   const page = await browser.newPage();
+  await page.setViewport({width: 1024, height: 768});
+  await page.goto('https://bedegaming.atlassian.net/plugins/servlet/ac/is.origo.jira.tempo-plugin/tempo-my-work#!/timesheet/', {waitUntil: 'networkidle'});
 
-  await browser.close();
-})();
+
+  await page.waitFor(5000);
+  browser.close();
+}
+run();
